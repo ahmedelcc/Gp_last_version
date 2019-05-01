2dd8461a7c95510c153e030936fed260dda0d122
Ahmed elcc <ahmedelcc733@gmail.com>
2019-05-01 21:01:04 +0200

    activity Recognition Gp

---
+++ app/src/main/java/com/example/alkdods/gp_last_version/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alkdods.gp_last_version;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
